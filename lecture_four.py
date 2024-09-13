83e611ece4be97027c4e9a006ae86d1beac326ce
kausu1012 <kaushalmina3@gmail.com>
2024-09-13 15:35:27 +0530

    Making it available to work

--- lecture_four.py
+++ lecture_four.py
@@ -1,12 +1,12 @@
-# marks = {}
+marks = {}
 
-# x = int(input("enter the phy marks : "))
-# marks.update({"physics":x})
+x = int(input("enter the phy marks : "))
+marks.update({"physics":x})
 
-# y = int(input("enter the phy marks : "))
-# marks.update({"chemistry":y})
+y = int(input("enter the phy marks : "))
+marks.update({"chemistry":y})
 
-# z = int(input("enter the phy marks : "))
-# marks.update({"mathmatics":z})
+z = int(input("enter the phy marks : "))
+marks.update({"mathmatics":z})
 
-# print(marks)
+print(marks)
